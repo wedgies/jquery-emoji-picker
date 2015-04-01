5710f3b7c425e2da3933e41703c959aa00143b96
Pawel Szymczykowski <makenai@gmail.com>
2015-04-01 11:32:43 -0700

    Updated bower.json, added grunt-bump to maintain version tags for bower

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    bump: {
+      options: {
+        files: ['package.json','bower.json'],
+        pushTo: 'origin'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-bump');
+
+};
