7e72878b5a0a79455ce58bf8451af03251103030
Pawel Szymczykowski <makenai@gmail.com>
2015-04-01 11:42:45 -0700

    Commiting both bower.json and package.json on bumps

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function(grunt) {
     bump: {
       options: {
         files: ['package.json','bower.json'],
+        commitFiles: ['package.json','bower.json'],
         pushTo: 'origin'
       }
     }
